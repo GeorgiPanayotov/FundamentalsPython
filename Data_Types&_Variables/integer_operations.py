99d9709783926e40ff630a986d84c7b78d80460b
GeorgiPanayotov <134947988+GeorgiPanayotov@users.noreply.github.com>
2023-06-04 18:00:09 +0300

    Create integer_operations.py
    Data_type_exercises

---
+++ Data_Types&_Variables/integer_operations.py
@@ -0,0 +1,44 @@
+# EXE 1
+#count = 0
+# lst = []
+# def zerofinder(x,y):
+#     try:
+#         return round(x / y)
+#     except ZeroDivisionError:
+#         return 0
+    
+# for i in range(0,4):
+#     number = int(input())
+#     lst.append(number)
+#     count += 1
+#     if count == 2: 
+#         add = sum(lst)
+#     elif count == 3:
+#         lst.remove(number)
+#         divide = zerofinder(add,number)
+#     elif count == 4:
+#         print(round(divide*number))
+
+#EXE 2
+# word = ''
+# for i in range(0,3):
+#     symbol = input()
+#     word = word + symbol
+# print(word)
+
+#EXE 3
+# people = int(input())
+# capacity = int(input())
+
+# if people % capacity != 0:
+#     add = people % capacity
+#     if add
+#     print((people // capacity) + add)
+# else:
+#     print(people // capacity)
+
+
+
+
+    
+
